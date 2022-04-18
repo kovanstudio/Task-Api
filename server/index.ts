2ad6aac97656fd9b7572f2ac3c247d0685bee1f8
burak94t <burak94t@gmail.com>
2022-04-18 19:49:25 +0300

    p

--- server/index.ts
+++ server/index.ts
@@ -39,7 +39,7 @@ app.get("/items", async (req, res) => {
     }
 
     for (let i = 0; i < json.data.bikes.length; i++) {
-      if (Math.random() > 0.98) {
+      if (Math.random() > 0.99) {
         json.data.bikes[i] = a[Math.floor(Math.random() * a.length)];
       } else {
         if (Math.random() > 0.9) {
